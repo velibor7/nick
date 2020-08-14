c725e04fbd3fd551245a07650b75c194922d4487
Velibor <veliborvasiljevic7@gmail.com>
2020-08-14 15:08:48 +0200

    code splitting :construction:

--- src/home/components/ImageItem.js
+++ src/home/components/ImageItem.js
@@ -1,8 +1,29 @@
 import React, { Fragment } from "react";
 
 import "./ImageItem.css";
-const ImageDetail = () => {
-  return <Fragment></Fragment>;
+const ImageDetail = (props) => {
+  return (
+    <li className="image-item__li-container">
+      <div className="image-item__inner-div">
+        <a className="image-item__link" href="#">
+          <div
+            className="image-item__img"
+            style={{
+              backgroundImage:
+                "url(" +
+                "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                ")",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <h1 className="image-item__title">{props.title}</h1>
+          <p className="image-item__description">{props.description}</p>
+        </a>
+      </div>
+    </li>
+  );
 };
 
 export default ImageDetail;
--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -2,10 +2,60 @@ import React, { Fragment } from "react";
 import MainNavigation from "../../shared/components/Navigation/MainNavigation";
 import ImageList from "../components/ImageList";
 const Home = () => {
+  const images = [
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      title: "Some title",
+      description: "Some desc",
+      img:
+        "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    },
+  ];
   return (
     <Fragment>
       <MainNavigation></MainNavigation>
-      <ImageList></ImageList>
+      <ImageList images={images}></ImageList>
     </Fragment>
   );
 };
